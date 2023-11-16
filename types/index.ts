629b8fdf519f8bbabefbff36825b750b56338e20
Hayden McDowall <haysay424@gmail.com>
2023-11-16 16:33:54 +1300

    Added header with logo and sign in button. Added footer with links, logo and copyright

--- types/index.ts
+++ types/index.ts
@@ -2,6 +2,7 @@ import { MouseEventHandler } from "react";
 
 export interface CustomButtonProps {
     title: string;
+    btnType?: "button" | "submit";
     containerStyles?: String;
     handleClick?: MouseEventHandler<HTMLButtonElement>;
 }
